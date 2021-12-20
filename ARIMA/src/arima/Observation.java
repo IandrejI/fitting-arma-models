40c00702d12ecca6daa3fe5421ced26ca886fb67
andre <andre@192.168.0.74>
2021-12-20 13:52:57 +0100

    Basic structure

---
+++ ARIMA/src/arima/Observation.java
@@ -0,0 +1,43 @@
+package arima;
+
+public class Observation {
+	
+	// Fields
+	private int index;
+	private double value;
+	private double prediction;
+	private double error;
+	
+	// Constructor
+	public Observation(int index, double value) {
+		this.index = index;
+		this.value = value;
+	}
+
+	
+	// Getters and Setters
+	public double getPrediction() {
+		return prediction;
+	}
+
+	public void setPrediction(double prediction) {
+		this.prediction = prediction;
+	}
+
+	public double getError() {
+		return error;
+	}
+
+	public void setError() {
+		this.error = value-prediction;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+}
